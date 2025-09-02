f4d8300c11bc244cce85a43303825236c604fc28
Stefan Blagojevic <blagojevics710@gmail.com>
2025-09-02 22:28:51 +0200

    added pass reset, delete comments and some user related changes

--- src/config/firebase.js
+++ src/config/firebase.js
@@ -1,25 +1,33 @@
 import { initializeApp } from "firebase/app";
 import { getAnalytics } from "firebase/analytics";
-import { getAuth } from "firebase/auth";
+import { getAuth, GoogleAuthProvider } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
+import { getStorage } from "firebase/storage";
 
 const firebaseConfig = {
   apiKey: "AIzaSyAQcdcOEmLA4PEVWtcUM5ehTmP2zMkbdfM",
-
   authDomain: "meowgram-cdd7c.firebaseapp.com",
-
   projectId: "meowgram-cdd7c",
-
   storageBucket: "meowgram-cdd7c.firebasestorage.app",
-
   messagingSenderId: "284043381940",
-
   appId: "1:284043381940:web:36f307344a02feaed81be1",
-
   measurementId: "G-0LJ1SX9W56",
 };
 
+// Initialize Firebase
 const app = initializeApp(firebaseConfig);
-export const auth = getAuth(app);
+
+// Analytics (optional, only works in browser with enabled analytics)
 const analytics = getAnalytics(app);
+
+// Auth
+export const auth = getAuth(app);
+
+// Firestore
 export const db = getFirestore(app);
+
+// Storage
+export const storage = getStorage(app);
+
+// Google Provider
+export const googleProvider = new GoogleAuthProvider();
